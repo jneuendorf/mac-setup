d2e0496fd9230c155c144a3a72d400277387eacb
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-03-23 22:30:07 +0100

    [backup] homebrew preparation

--- backup.sh
+++ backup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# cwd=$(pwd)
 # https://stackoverflow.com/a/8488293/6928824
 
 
@@ -23,39 +22,22 @@ done
 
 echo "
 >> Running backup preparation scripts..."
-
+./src/backup/homebrew.sh
 ./src/backup/vscode-extensions.sh
 
 
 # FIND OUT WHICH MACKUP BINARY TO USE
-
-# cp -f src/mackup.cfg ~/.mackup.cfg
-# mackup_config="$(cat src/mackup.cfg) $(sed 's/ - //g' <<< $(pipenv run mackup list | grep -- '- '))"
-# echo $mackup_config
-# # mackup_apps=`sed 's/ - //g' <<< $(pipenv run mackup list | grep -- '- ')`
-# exit 0
-
 if [ $(which mackup) ]; then
     mackup_bin=$(which mackup)
-    # apps_to_sync=$(sed 's/ - //g' <<< $(mackup list | grep -- '- '))
-    # sed -i "s/__apps__/$apps_to_sync/" ~/.mackup.cfg
-
-    # mackup backup
 else
     cd mackup
-    # pipenv install
-    # pipenv run make develop
-
     mackup_bin="pipenv run mackup"
-
-    # pipenv run mackup backup
 fi
 
 
 echo "
 >> Generating mackup config..."
 apps_to_sync=$(sed 's/ - //g' <<< $($mackup_bin list | grep -- '- '))  # remove ' - ' from all list items
-# sed -i "s/__apps__/$apps_to_sync/" ~/.mackup.cfg
 for app in $apps_to_sync
 do
     echo "$app" >> ~/.mackup.cfg
