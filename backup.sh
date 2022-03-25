5698b1228a0530f19ed08f8692377b51c434ba59
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-03-25 23:47:50 +0100

    enhance backup, extend ssh config, extend macos config

--- backup.sh
+++ backup.sh
@@ -1,29 +1,48 @@
 #!/usr/bin/env bash
 
-# https://stackoverflow.com/a/8488293/6928824
+
+mackup_cfg="$1"
+
+if [[ "$mackup_cfg" != "" ]]; then
+    mackup_cfg="$1"
+    if [ -f ~/.vscode_extensions ]; then
+        #
+    else
+        echo "Invalid mackup config path '$1'. Exiting..."
+        exit 1
+    fi
+else
+    mackup_cfg=src/mackup.cfg
+fi
+
 
 
 echo "
 >> Copying mackup config stub..."
-cp src/mackup.cfg ~/.mackup.cfg
+
+cp "$mackup_cfg" ~/.mackup.cfg
+
 
 
 echo "
 >> Copying application configs..."
 
 mkdir -p ~/.mackup/
-for config in $(ls src/configs/)
+for cfg in $(ls src/configs/)
 do
-    echo "copying src/configs/$config -> ~/.mackup/$config"
-    # cp -v -f "src/configs/$config" "~/.mackup/"
-    cp "src/configs/$config" ~/.mackup
+    echo "copying src/configs/$cfg -> ~/.mackup/$cfg"
+    cp "src/configs/$cfg" ~/.mackup
 done
+
 
 
 echo "
 >> Running backup preparation scripts..."
+
 ./src/backup/homebrew.sh
 ./src/backup/vscode-extensions.sh
+# TODO: defaults etc
+
 
 
 # FIND OUT WHICH MACKUP BINARY TO USE
@@ -31,12 +50,17 @@ if [ $(which mackup) ]; then
     mackup_bin=$(which mackup)
 else
     cd mackup
+    # pipenv install
+    # pipenv run make develop
     mackup_bin="pipenv run mackup"
 fi
 
 
+
 echo "
->> Generating mackup config..."
+>> Finalizing mackup config (applications_to_sync)..."
+
+echo "[applications_to_sync]" >> ~/.mackup.cfg
 apps_to_sync=$(sed 's/ - //g' <<< $($mackup_bin list | grep -- '- '))  # remove ' - ' from all list items
 for app in $apps_to_sync
 do
@@ -44,6 +68,13 @@ do
 done
 
 
+
 echo "
 >> Running mackup..."
+
 $mackup_bin backup
+
+
+# TODO
+# echo "
+# >> Generating standalone restore binary"
